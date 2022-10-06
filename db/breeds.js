0ef65bc79f8d2576a9679b3ff9e4e63429f141c8
Trey <treybyars93@gmail.com>
2022-10-06 10:22:42 -0400

    wrote getBreedByName and createBreed funcs

--- db/breeds.js
+++ db/breeds.js
@@ -8,9 +8,46 @@ const getAllProductsByBreedId = async (id) => {
       WHERE id=$1;
       `,[id]);
 
+    console.log(breed)
     return breed;
   } catch (error) {
     console.log('error getting products by breed id');
     throw error;
   }
+}
+
+const getBreedByName = async (name) => {
+  try {
+    const { rows: [breed] } = await client.query(`
+      SELECT *
+      FROM breeds
+      WHERE name=$1;
+      `,[name]);
+
+    return breed;
+  } catch (error) {
+    console.log('error getting products by breed name');
+    throw error;
+  }
+}
+
+const createBreed = async ({name}) => {
+  try {
+    const { rows: [breed] } = await client.query(`
+    INSERT INTO breeds (name)
+    VALUES ($1)
+    RETURNING *;
+    `, [name]);
+    
+    return breed;
+  } catch (error) {
+    console.log('error creating breed')
+    throw error;
+  }
+}
+
+module.exports = {
+  getAllProductsByBreedId,
+  getBreedByName,
+  createBreed
 }
