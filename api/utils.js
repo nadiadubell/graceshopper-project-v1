f6579fc7cf7efff0ae23841920922067dec55419
nadiadubell <ndubell01@gmail.com>
2022-10-09 19:20:46 -0500

    added dotenv npm to fix issue with users/login route

--- api/utils.js
+++ api/utils.js
@@ -1,28 +1,27 @@
 const requireAdmin = (req, res, next) => {
-
-  if(!req.isAdmin) {
+  if (!req.isAdmin) {
     res.statusCode = 401;
     next({
       name: 'MissingAdminCredentialsError',
-      message: 'You must be an administrator to perform this action'
-    })
+      message: 'You must be an administrator to perform this action',
+    });
   }
 
   next();
-}
+};
 
 const requireUser = (req, res, next) => {
   if (!req.user) {
     res.statusCode = 401;
     next({
       name: 'MissingUserCredentialsError',
-      message: 'You must be a registered user to perform this action'
-    })
+      message: 'You must be a registered user to perform this action',
+    });
   }
   next();
-}
+};
 
 module.exports = {
   requireAdmin,
-  requireUser
-}
+  requireUser,
+};
