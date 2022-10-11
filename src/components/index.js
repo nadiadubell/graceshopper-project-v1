bff29a9021d24409851c046cac331abe512a3277
Jerrod Stafford <jerrodstafford10@gmail.com>
2022-10-11 14:03:46 -0500

    Created register/login pages, cleaned up front-end code

--- src/components/index.js
+++ src/components/index.js
@@ -3,3 +3,7 @@ export { Header } from './Header';
 export { Footer } from './Footer';
 
 export { Profile } from './Profile';
+
+export { Register } from './Register';
+
+export { Login } from './Login';
