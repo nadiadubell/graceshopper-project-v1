67ce8e79be5cbb380ea6c919e835aee83e3cadde
The-Gammelier <aaronsexton5@gmail.com>
2022-10-12 14:01:01 -0500

    Pulling from main

--- src/components/Orders.js
+++ src/components/Orders.js
@@ -45,45 +45,48 @@ export const Orders = () => {
     <div>
       {userOrder.map((order, i) => {
         return (
-          <div key={i}>
-            {order.products.map((product, i) => {
-              return (
-                <div key={i}>
-                  <img src={product.image} />
-                  <h4>{product.name}</h4>
-                  <ul>
-                    <li>Price: {product.price}</li>
-                    <span>
-                      <li>Quantity:</li>
-                      <select required>
-                        <option value="quantity">{product.quantity}</option>
-                        <option value={1}>1</option>
-                        <option value={2}>2</option>
-                        <option value={3}>3</option>
-                        <option value={4}>4</option>
-                        <option value={5}>5</option>
-                        <option value={6}>6</option>
-                        <option value={7}>7</option>
-                        <option value={8}>8</option>
-                        <option value={9}>9</option>
-                        {/* need to figure out how to make an input field that allows you
+          <div id="order-page">
+            <div key={i} id="order-products">
+              {order.products.map((product, i) => {
+                return (
+                  <div key={i}>
+                    <img src={product.image} />
+                    <h4>{product.name}</h4>
+                    <ul>
+                      <li>Price: {product.price}</li>
+                      <span>
+                        <li>Quantity:</li>
+                        <select required>
+                          <option value="quantity">{product.quantity}</option>
+                          <option value={1}>1</option>
+                          <option value={2}>2</option>
+                          <option value={3}>3</option>
+                          <option value={4}>4</option>
+                          <option value={5}>5</option>
+                          <option value={6}>6</option>
+                          <option value={7}>7</option>
+                          <option value={8}>8</option>
+                          <option value={9}>9</option>
+                          {/* need to figure out how to make an input field that allows you
                         to manually enter a number larger than 10 if the below is selected */}
-                        <option value={10}>10+</option>
-                      </select>
-                    </span>
-                    <br></br>
-                    <button
-                      onClick={async event => {
-                        event.preventDefault();
-                        handleRemoveButtonClick(order.id, product.id);
-                      }}
-                    >
-                      Remove From Cart
-                    </button>
-                  </ul>
-                </div>
-              );
-            })}
+                          <option value={10}>10+</option>
+                        </select>
+                      </span>
+                      <br></br>
+                      <button
+                        onClick={async event => {
+                          event.preventDefault();
+                          handleRemoveButtonClick(order.id, product.id);
+                        }}
+                      >
+                        Remove From Cart
+                      </button>
+                    </ul>
+                  </div>
+                );
+              })}
+            </div>
+            <div id="order-summary"></div>
           </div>
         );
       })}
