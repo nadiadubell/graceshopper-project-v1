0720f6edcf2baa31ce2a40297deac62be5eb6602
Trey <treybyars93@gmail.com>
2022-10-12 11:37:11 -0400

    removed func from api/index

--- src/api/index.js
+++ src/api/index.js
@@ -1,6 +1,3 @@
 export const BASE = 'http://localhost:4000/api'
 
-export const getOrderHistoryById = async(userId) => {
-  const userOrderHistory = await axios.get(`${BASE}/users/:username/profile`)
-}
 
