0c4370dc51bebf8dcf4defd5c422a897c9427aea
nadiadubell <ndubell01@gmail.com>
2022-10-13 12:49:39 -0500

    still working on css for admin

--- src/components/Admin.js
+++ src/components/Admin.js
@@ -26,20 +26,22 @@ export const Admin = () => {
     getUsers(token);
   }, []);
   return (
-    <div id="admin-dashboard">
-      <h1 id="admin-header">Welcome Admin!</h1>
-      <h2 id="admin-dashboard-header">Dashboard</h2>
-      {users.map((user, id) => {
-        return (
-          <div id="users" key={id}>
-            <h3 id="user-username">username: {user.username}</h3>
-            <h3 id="user-isAdmin">isAdmin: {String(user.isAdmin)}</h3>
-            <h3 id="user-firstname">firstname: {user.firstName}</h3>
-            <h3 id="user-lastname">lastname: {user.lastName}</h3>
-            <h3 id="user-email">email: {user.email}</h3>
-          </div>
-        );
-      })}
+    <div id="admin-page">
+      <h1 id="admin-title">Welcome Admin!</h1>
+      <h2 id="dashboard-title">Dashboard</h2>
+      <div id="users-container">
+        {users.map((user, id) => {
+          return (
+            <div id="users" key={id}>
+              <h3 id="user-username">username: {user.username}</h3>
+              <h3 id="user-isAdmin">isAdmin: {String(user.isAdmin)}</h3>
+              <h3 id="user-firstname">firstname: {user.firstName}</h3>
+              <h3 id="user-lastname">lastname: {user.lastName}</h3>
+              <h3 id="user-email">email: {user.email}</h3>
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
