a033cfa377e3ec2b6f1fb8d2c805683230fa55fd
The-Gammelier <aaronsexton5@gmail.com>
2022-10-21 20:52:00 -0500

    exact

--- src/index.js
+++ src/index.js
@@ -62,6 +62,7 @@ const App = () => {
           }
         ></Route>
         <Route
+          exact
           path="/products"
           element={
             <Products isLoggedIn={isLoggedIn} setProductId={setProductId} />
