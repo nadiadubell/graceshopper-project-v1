44319a11fa4deecee16b00d4a2c2e8fa622d0e87
nadiadubell <ndubell01@gmail.com>
2022-12-05 15:57:42 -0600

    changed base url in index.js

--- src/api/index.js
+++ src/api/index.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 //   ? `${process.env.BASE}:${process.env.PORT}/api`
 //   : 'http://localhost:4000/api';
 
-export const BASE = '/api';
+export const BASE = 'https://horseplay-site.onrender.com/api';
 //   process.env.NODE_ENV === 'production'
 //     ? `https://horseplay.herokuapp.com:443/api`
 //     : 'http://localhost:4000/api';
