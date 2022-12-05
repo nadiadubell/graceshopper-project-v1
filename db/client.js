ab7e7a5f4bfd4849d21707d24bc18cd7eac6895f
nadiadubell <ndubell01@gmail.com>
2022-12-05 16:38:10 -0600

    changed back client.js file

--- db/client.js
+++ db/client.js
@@ -1,18 +1,18 @@
 const { Pool, Client } = require('pg');
-// const connectionString =
-//   process.env.DATABASE_URL || 'http://localhost:5432/horseplay-dev';
+const connectionString =
+  process.env.DATABASE_URL || 'http://localhost:5432/horseplay-dev';
 
 const DATABASE_URL = process.env.DB_URL;
 const PASSWORD = process.env.DB_PW;
 
-const client = new Pool({
-  user: 'ndubell01',
-  host: 'db.bit.io',
-  database: DATABASE_URL,
-  password: PASSWORD, // key from bit.io database page connect menu
-  port: 5432,
-  ssl: true,
-});
+// const client = new Pool({
+//   user: 'ndubell01',
+//   host: 'db.bit.io',
+//   database: DATABASE_URL,
+//   password: PASSWORD, // key from bit.io database page connect menu
+//   port: 5432,
+//   ssl: true,
+// });
 
 // const client = new Pool({
 //   connectionString,
@@ -24,14 +24,14 @@ const client = new Pool({
 //       : undefined,
 // });
 
-// const client = new Client(
-// {
-// connectionString
-//   ssl:
-//     process.env.NODE_ENV === 'production'
-//       ? { rejectUnauthorized: false }
-//       : undefined,
-// }
-// );
+const client = new Client(
+  // {
+  connectionString
+  //   ssl:
+  //     process.env.NODE_ENV === 'production'
+  //       ? { rejectUnauthorized: false }
+  //       : undefined,
+  // }
+);
 
 module.exports = client;
