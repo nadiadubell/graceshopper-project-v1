f4d6af7993c1d08527de020207d9990ae841ef24
nadiadubell <ndubell01@gmail.com>
2023-02-16 15:08:57 -0600

    changing connection

--- db/client.js
+++ db/client.js
@@ -1,18 +1,19 @@
+require('dotenv').config();
 const { Pool, Client } = require('pg');
-// const connectionString =
-//   process.env.DATABASE_URL || 'http://localhost:5432/horseplay-dev';
+const connectionString =
+  process.env.DATABASE_URL || 'http://localhost:5432/horseplay-dev';
 
 const NAME = process.env.DB_NAME;
 const PASSWORD = process.env.DB_PW;
 
-const client = new Pool({
-  user: 'ndubell01',
-  host: 'db.bit.io',
-  database: NAME,
-  password: PASSWORD, 
-  port: 5432,
-  ssl: true,
-});
+// const client = new Pool({
+//   user: 'ndubell01',
+//   host: 'db.bit.io',
+//   database: NAME,
+//   password: PASSWORD, 
+//   port: 5432,
+//   ssl: true,
+// });
 
 // const client = new Pool({
 //   connectionString,
@@ -24,14 +25,14 @@ const client = new Pool({
 //       : undefined,
 // });
 
-// const client = new Client(
+const client = new Client(
   // {
-  // connectionString
+  connectionString
   //   ssl:
   //     process.env.NODE_ENV === 'production'
   //       ? { rejectUnauthorized: false }
   //       : undefined,
   // }
-// );
+);
 
 module.exports = client;
