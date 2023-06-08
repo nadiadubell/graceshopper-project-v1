671a21cd74b6174be5bdf7943eab8aa457c4967e
nadiadubell <ndubell01@gmail.com>
2023-06-08 12:50:51 -0500

    switched connection in client.js file

--- db/client.js
+++ db/client.js
@@ -1,19 +1,19 @@
 require('dotenv').config();
 const { Pool, Client } = require('pg');
-const connectionString =
-  process.env.DATABASE_URL || 'http://localhost:5432/horseplay-dev';
+// const connectionString =
+//   process.env.DATABASE_URL || 'http://localhost:5432/horseplay-dev';
 
 const NAME = process.env.DB_NAME;
 const PASSWORD = process.env.DB_PW;
 
-// const client = new Pool({
-//   user: 'ndubell01',
-//   host: 'db.bit.io',
-//   database: NAME,
-//   password: PASSWORD, 
-//   port: 5432,
-//   ssl: true,
-// });
+const client = new Pool({
+  user: 'postgres',
+  host: 'db.gjtbyfkrwxbwplglkcre.supabase.co',
+  database: NAME,
+  password: PASSWORD,
+  port: 5432,
+  ssl: true,
+});
 
 // const client = new Pool({
 //   connectionString,
@@ -25,14 +25,14 @@ const PASSWORD = process.env.DB_PW;
 //       : undefined,
 // });
 
-const client = new Client(
-  // {
-  connectionString
-  //   ssl:
-  //     process.env.NODE_ENV === 'production'
-  //       ? { rejectUnauthorized: false }
-  //       : undefined,
-  // }
-);
+// const client = new Client(
+// {
+// connectionString
+//   ssl:
+//     process.env.NODE_ENV === 'production'
+//       ? { rejectUnauthorized: false }
+//       : undefined,
+// }
+// );
 
 module.exports = client;
